ceaa6f39723c2daf969870b475a7c278cd30e365
MurrayDylan <71659904+MurrayDylan@users.noreply.github.com>
2026-07-08 20:44:21 +0100

    Added repository layers

--- backend/src/main/java/ie/dylanmurray/website/repository/ProjectRepository.java
+++ backend/src/main/java/ie/dylanmurray/website/repository/ProjectRepository.java
@@ -1,4 +1,13 @@
 package ie.dylanmurray.website.repository;
 
-public class ProjectRepository {
+import ie.dylanmurray.website.entity.Project;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+
+@Repository
+public interface ProjectRepository
+        extends JpaRepository<Project, Long> {
+
+
 }
--- backend/src/main/java/ie/dylanmurray/website/repository/TechnologyRepository.java
+++ backend/src/main/java/ie/dylanmurray/website/repository/TechnologyRepository.java
@@ -1,4 +1,17 @@
 package ie.dylanmurray.website.repository;
 
-public class TechnologyRepository {
+import ie.dylanmurray.website.entity.Technology;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+
+@Repository
+public interface TechnologyRepository
+        extends JpaRepository<Technology, Long> {
+
+
+    Optional<Technology> findByName(String name);
+
 }
