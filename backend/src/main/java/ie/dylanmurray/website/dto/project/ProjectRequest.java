164118b6f2bb1c3e9aeb7c9f4dcf0b3f7c096825
MurrayDylan <71659904+MurrayDylan@users.noreply.github.com>
2026-07-16 15:14:37 +0100

    better error responses, added openAPI

--- backend/src/main/java/ie/dylanmurray/website/dto/project/ProjectRequest.java
+++ backend/src/main/java/ie/dylanmurray/website/dto/project/ProjectRequest.java
@@ -2,20 +2,23 @@ package ie.dylanmurray.website.dto.project;
 
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.Size;
 
 import java.util.List;
 
 public class ProjectRequest {
 
     @NotBlank(message = "Title cannot be empty")
+    @Size(max = 100, message = "Title cannot exceed 100 characters")
     private String title;
 
+    @Size(max = 3000, message = "Description cannot exceed 3000 characters")
     private String description;
 
     private String projectUrl;
 
     @NotEmpty(message = "Technologies must have atleast one technology")
-    private List<String> technologies;
+    private List<@NotBlank String> technologies;
 
     public ProjectRequest() {
     }
--- backend/src/main/java/ie/dylanmurray/website/exception/ApiError.java
+++ backend/src/main/java/ie/dylanmurray/website/exception/ApiError.java
@@ -1,6 +1,7 @@
 package ie.dylanmurray.website.exception;
 
 import java.time.LocalDateTime;
+import java.util.Map;
 
 public class ApiError {
 
@@ -8,6 +9,7 @@ public class ApiError {
     private LocalDateTime timestamp;
     private int status;
     private String message;
+    private Map<String, String> fieldErrors;
 
 
     public ApiError(
@@ -20,15 +22,36 @@ public class ApiError {
         this.message = message;
     }
 
+
+    public ApiError(
+            LocalDateTime timestamp,
+            int status,
+            String message,
+            Map<String, String> fieldErrors
+    ) {
+        this.timestamp = timestamp;
+        this.status = status;
+        this.message = message;
+        this.fieldErrors = fieldErrors;
+    }
+
+
     public LocalDateTime getTimestamp() {
         return timestamp;
     }
+
 
     public int getStatus() {
         return status;
     }
 
+
     public String getMessage() {
         return message;
+    }
+
+
+    public Map<String, String> getFieldErrors() {
+        return fieldErrors;
     }
 }
